48c55d9999f688af3fbdf86cac8d44fcc2ee9327
abhay <abhay@HPC>
2021-01-31 22:32:01 -0600

    .........

--- Api/MyFunction.cs
+++ Api/MyFunction.cs
@@ -12,24 +12,33 @@ namespace BlazorApp.Api
 {
     public static class MyFunction
     {
+        //[FunctionName("MyFunction")]
+        //public static async Task<IActionResult> Run(
+        //    [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        //    ILogger log)
+        //{
+        //    log.LogInformation("C# HTTP trigger function processed a request.");
+
+        //    string name = req.Query["name"];
+
+        //    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        //    dynamic data = JsonConvert.DeserializeObject(requestBody);
+        //    name = name ?? data?.name;
+
+        //    string responseMessage = string.IsNullOrEmpty(name)
+        //        ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+        //        : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+        //    return new OkObjectResult(responseMessage);
+        //}
+
         [FunctionName("MyFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static IActionResult Run(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+    ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+           return new OkObjectResult("Abhay");
         }
     }
 }
+
